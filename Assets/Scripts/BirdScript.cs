3d59f7405169dbbf5cef1c66e9d33f08be3c3d81
Denys Samoilenko <denniksam@gmail.com>
2023-11-21 13:19:09 +0200

    Colliders

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private float descreteForceFactor  = 250f;
+    private float continualForceFactor = 1000f;
+
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  // "імпульс" - лише на натиснення
+        {
+            body.AddForce(Vector2.up * descreteForceFactor);
+        }
+
+        if(Input.GetKey(KeyCode.W))  // "неперервний" - з кожним Update
+        {
+            body.AddForce(Vector2.up * continualForceFactor * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Debug.Log("Collision detected: " +  collision.gameObject.name);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug.Log("Trigger detected: " + collision.gameObject.name);
+    }
+}
+/* З точки зору взаємодії між собою колайдери поділяються на фізичні та тригери
+ * 
+ * Фізичні колайдери "відпрацьовують" зіткнення засобами двигуна Юніті та
+ * передають у скріпт подію OnCollisionEnter2D
+ * 
+ * Тригер-колайдери не беруть участь у двигуні, лише передають подію
+ * OnTriggerEnter2D
+ * 
+ * Події взаємно виключні - або тригер, або колізія. Не обидві відразу,
+ * навіть якщо один з колайдерів фізичний, а інший тригер.
+ * 
+ * Подію отримує кожен скрипт, що бере участь у зіткненні. Якщо один з
+ * колайдерів фізичний, інший тригер -- обидва отримують подію "Тригер"
+ * 
+ * Хоча б один з учасників зіткнення повинен мати компонент Rigidbody,
+ * іншакше колізії не детектуються. (Відсутність Rigidbody - значить
+ * об'єкт не бере участь у фізичному циклі)
+ */
---
+++ Assets/Scripts/SpawnPointScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pipePrefab;
+
+    private float pipeSpawnPeriod = 4f;   // час у секундах між появою труб
+    private float pipeCountdown;          // залишок часу до появи
+
+    void Start()
+    {
+        pipeCountdown = pipeSpawnPeriod;
+        SpawnPipe();
+    }
+
+    void Update()
+    {
+        pipeCountdown -= Time.deltaTime;
+        if(pipeCountdown <= 0)
+        {
+            pipeCountdown = pipeSpawnPeriod;
+            SpawnPipe();
+        }
+    }
+
+    private void SpawnPipe()
+    {
+        var pipe = GameObject.Instantiate(pipePrefab);   // ~ new pipePrefab
+        pipe.transform.position = 
+            this.transform.position
+            + Vector3.up * Random.Range(-1.4f, 1.4f);
+    }
+}
+/* Д.З. Створити ще один префаб труби зі зменшеним/збільшеним
+ * проміжком. Модифікувати скрипт появи труб на випадковий 
+ * вибір одного з префабів.
+ * ** Зробити декілька таких префабів
+ */
